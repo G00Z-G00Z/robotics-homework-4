916b225e07329aea8c8ac4883d933ae8468bad0c
G00Z_G00Z <edygomezgg4@gmail.com>
2023-03-01 14:43:31 -0600

    Added angle

--- src/lib.py
+++ src/lib.py
@@ -50,3 +50,40 @@ def get_homogenous_matrix_from_len_angle(angle: float, length: float) -> ndarray
     # Block puts the vectors or matrixes together as long as dimension match
     return np.block([[rot, initial_position], [np.zeros((1, 2)), 1]])
 
+
+def solve_final_angle(arms: list[Arm]) -> ndarray:
+    transform_matrices = [
+        get_homogenous_matrix_from_len_angle(a.theta, a.length) for a in arms
+    ]
+
+    for idx, mat in enumerate(transform_matrices):
+        print(f"T{idx + 1}: ")
+        print(mat)
+
+    # Reverse ?? 
+    final_matrix = reduce(lambda prev, next: prev @ next, transform_matrices)
+
+    print("Final Matrix: ")
+    print(final_matrix)
+    print("Final Coord: ")
+    coord = final_matrix[0:2, 2]
+    print(final_matrix[0:2, 2])
+    return coord
+
+
+if __name__ == "__main__":
+    arms_no = 3
+
+    arms = [Arm() for _ in range(arms_no)]
+
+    arms_length = 0.04
+
+    arms[0].length = arms_length
+    arms[0].theta = 30
+    arms[1].length = arms_length
+    arms[1].theta = 30
+    arms[2].length = arms_length
+    arms[2].theta = -55
+
+    solve_final_angle(arms)
+    pass
